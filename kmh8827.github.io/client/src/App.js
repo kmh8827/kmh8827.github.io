7438ea59d5a7049c7f09f5a2612868e8076780d9
kevin <kevinhellman94@gmail.com>
2021-04-25 21:44:54 -0400

    Created React Router

--- kmh8827.github.io/client/src/App.js
+++ kmh8827.github.io/client/src/App.js
@@ -1,8 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "../src/components/Footer";
+import Header from "../src/components/Header";
+import Homepage from "../src/pages/Homepage";
+import Contact from "../src/pages/Contact";
+import Portfolio from "../src/pages/Portfolio";
+import NoMatch from "../src/pages/NoMatch";
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path={["/"]}>
+            <Homepage />
+          </Route>
+          <Route exact path={["/portfolio"]}>
+            <Portfolio />
+          </Route>
+          <Route exact path={["/Contact"]}>
+            <Contact />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
