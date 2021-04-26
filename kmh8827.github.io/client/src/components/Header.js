d8c863f94cb5eb9ea7e87a400c01079bafde02ab
kevin <kevinhellman94@gmail.com>
2021-04-26 13:25:52 -0400

    Updating Navigation Bar

--- kmh8827.github.io/client/src/components/Header.js
+++ kmh8827.github.io/client/src/components/Header.js
@@ -2,11 +2,28 @@ import React from "react";
 
 function Header() {
     return (
-        <nav className="nav">
-            <a href="/">HomePage</a>
-            <a href="/portfolio">Portfolio</a>
-            <a href="/contact">contact</a>
-        </nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <p class="navbar-brand">Kevin Hellman</p>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="nav navbar-nav ">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.html">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="portfolio.html">Portfolio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
 }
 
