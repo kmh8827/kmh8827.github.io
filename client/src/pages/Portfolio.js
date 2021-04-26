4bb260cc7a9ac40e0a25dd638e1dcf47d49f91e4
kevin <kevinhellman94@gmail.com>
2021-04-26 14:50:29 -0400

    Formated Portfolio Projects

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,4 +1,5 @@
 import Project from "../components/Project";
+import TravelBuddy from "../assets/project1.png";
 
 function Portfolio() {
     return (
@@ -9,10 +10,68 @@ function Portfolio() {
                         <h1 class="headingStyle">Portfolio</h1>
                         <hr />
                     </div>
+                </div>
+
+                <div class="row">
 
                     <div class="col">
                         <Project
-                            image="../assets/project1.png"
+                            image={TravelBuddy}
+                            alt="picture of a travel assistance computer application"
+                            github="https://github.com/kmh8827/project1-team2"
+                            deploy="https://kmh8827.github.io/project1-team2/"
+                            text="Link to Deployed Travel Buddy Application"
+                        />
+                    </div>
+
+                    <div class="col">
+                        <Project
+                             image={TravelBuddy}
+                            alt="picture of a travel assistance computer application"
+                            github="https://github.com/kmh8827/project1-team2"
+                            deploy="https://kmh8827.github.io/project1-team2/"
+                            text="Link to Deployed Travel Buddy Application"
+                        />
+                    </div>
+
+                </div>
+
+                <div class="row">
+                <div class="col">
+                        <Project
+                             image={TravelBuddy}
+                            alt="picture of a travel assistance computer application"
+                            github="https://github.com/kmh8827/project1-team2"
+                            deploy="https://kmh8827.github.io/project1-team2/"
+                            text="Link to Deployed Travel Buddy Application"
+                        />
+                    </div>
+
+                    <div class="col">
+                        <Project
+                             image={TravelBuddy}
+                            alt="picture of a travel assistance computer application"
+                            github="https://github.com/kmh8827/project1-team2"
+                            deploy="https://kmh8827.github.io/project1-team2/"
+                            text="Link to Deployed Travel Buddy Application"
+                        />
+                    </div>
+                </div>
+
+                <div class="row">
+                <div class="col">
+                        <Project
+                             image={TravelBuddy}
+                            alt="picture of a travel assistance computer application"
+                            github="https://github.com/kmh8827/project1-team2"
+                            deploy="https://kmh8827.github.io/project1-team2/"
+                            text="Link to Deployed Travel Buddy Application"
+                        />
+                    </div>
+
+                    <div class="col">
+                        <Project
+                             image={TravelBuddy}
                             alt="picture of a travel assistance computer application"
                             github="https://github.com/kmh8827/project1-team2"
                             deploy="https://kmh8827.github.io/project1-team2/"
