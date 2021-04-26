ba05ed85e949d87a8c8d34a1b11ed7da8c27e139
kevin <kevinhellman94@gmail.com>
2021-04-25 20:43:40 -0400

    Created Basic Header-Navigation

---
+++ kmh8827.github.io/client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <nav className="nav">
+            <a href="/">HomePage</a>
+            <a href="/portfolio">Portfolio</a>
+            <a href="/contact">contact</a>
+        </nav>
+    )
+}
+
+export default Header;
