5327e182f743eb564561355d5df88e5c3bdf3895
kevin <kevinhellman94@gmail.com>
2021-04-26 13:17:01 -0400

    Updated Home Page

---
+++ kmh8827.github.io/client/src/pages/Homepage.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div>
+            <main class="container bg-dark">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="page-header">
+                            <h1 class="headingStyle">About Me</h1>
+                        </div>
+                        <hr />
+                        <div class="row">
+                            <section class="col-md-6">
+                                <img src="assets/Self-PictureSmall.jpg" height="250px" width="300px" alt=""/>
+                            </section>
+                            <aside class="col-md-6">
+                                <p class="content">My name is Kevin Hellman. I was born in Durham, North Carolina, but moved shortly after to Apex. I went to Apex elementary, middle, and high school.</p>
+                                <p class="content">I graduated from N.C. State University with a bachelor's degree in Psychology. During the Covid-19 Pandemic I saw an opportunity to go back to school and learn programming.</p>
+                                <p class="content">I attend a bootcamp from UNC Chapel Hill at the moment.</p>
+                                <p class="content">If you would like to reach me you can do so at kevinhellman94@gmail.com</p>
+                            </aside>
+                        </div>
+                    </div>
+                </div>
+                <br />
+            </main>
+        </div>
+    )
+}
+
+export default HomePage;
