e428a80ffb44cf94fdef63b146b69967976bb89d
kevin <kevinhellman94@gmail.com>
2021-04-26 13:47:34 -0400

    Updated Portfolio to display project 1

--- kmh8827.github.io/client/src/pages/Portfolio.js
+++ kmh8827.github.io/client/src/pages/Portfolio.js
@@ -1,2 +1,29 @@
 import Project from "../components/Project";
 
+function Portfolio() {
+    return (
+        <div>
+            <main class="container bg-dark">
+                <div class="row">
+                    <div class="page-header">
+                        <h1 class="headingStyle">Portfolio</h1>
+                        <hr />
+                    </div>
+
+                    <div class="col">
+                        <Project
+                            image="../assets/project1.png"
+                            alt="picture of a travel assistance computer application"
+                            github="https://github.com/kmh8827/project1-team2"
+                            deploy="https://kmh8827.github.io/project1-team2/"
+                            text="Link to Deployed Travel Buddy Application"
+                        />
+                    </div>
+                </div>
+
+            </main>
+        </div>
+    );
+};
+
+export default Portfolio;
