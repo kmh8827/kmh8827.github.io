2239f390a1871b9f990584f68d3d804e269ff7bc
kevin <kevinhellman94@gmail.com>
2021-04-25 20:44:56 -0400

    Added footer component

---
+++ kmh8827.github.io/client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <p>© Copyright Kevin Hellman 2020</p>
+        </footer>
+    )
+}
+
+export default Footer;
