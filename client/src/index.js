20b86a22dc54c3375f95fdb09276ac9ba287a983
kevin <kevinhellman94@gmail.com>
2021-04-27 14:37:51 -0400

    Fixing background color

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+document.body.style = 'background:  #56a0d3;'
+
 ReactDOM.render(<App />, document.getElementById("root"));
